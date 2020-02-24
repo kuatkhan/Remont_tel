0d67e255ba3072272526deb64bbb84c61c994fae
kuatkhan <kuat88kz1@gmail.com>
2020-02-24 23:31:38 +0600

    create models

--- remont_tel/models.py
+++ remont_tel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+        name = models.CharField('Название категории', max_length=120)
+        slug = models.SlugField('URL', max_length=120)
+        text = models.TextField('Текст категории')
+        banner = models.ImageField('Баннер', upload_to='images/', blank=True, null=True)
+        title = models.CharField('Title', max_length=120)
+        description = models.CharField('Description', max_length=250)
+        keywords = models.CharField('Keywords', max_length=250)
